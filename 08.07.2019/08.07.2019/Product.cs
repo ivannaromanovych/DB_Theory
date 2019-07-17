6f57e4e3b908d0ee4b44275ea20b3f888293be59
Romanovych <ivankaromanovych12@gmail.com>
2019-07-17 20:48:01 +0300

    added all

---
+++ 08.07.2019/08.07.2019/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08._07._2019
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public bool isAvaliable { get; set; }
+        public DateTime DateCreate { get; set; }
+        public string Description { get; set; }
+        private string _image;
+        public string Image
+        {
+            get
+            {
+                return _image;
+            }
+            set
+            {
+                BitmapImage = new BitmapImage(new Uri(Environment.CurrentDirectory + "//Images//" + value));
+                _image = value;
+            }
+        }
+        public float Price { get; set; }
+
+        public BitmapImage BitmapImage { get; set; }
+    }
+}
